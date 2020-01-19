075f274ee00d897c190cf811dd86d9c7db728334
Anika Corpus <anika.corpus@coxautoinc.com>
2020-01-18 16:43:00 -0800

    add react-mdl

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-mdl';
 
 const App = (props) => {
   const [memeUrl, setMemeUrl] = useState('');
 
   const fetchData = async () => {
-    const response = await fetch("https://api.giphy.com/v1/gifs/random?rating=PG-13&tag=funny&api_key=kdGBXeHVs1fOu0W6YmBtQxvlO6tsYzBs");
+    const response = await fetch(   "https://media1.giphy.com/media/qaaSZMnWkufRu/200.webp?cid=790b7611b4b541fff75014c864546653ef73507d0ca8a49f&rid=200.webp",);
     response.json().then((responseObj) => {
+        console.log(responseObj)
       setMemeUrl(responseObj.data.image_original_url);
     })
   };
@@ -21,7 +23,7 @@ const App = (props) => {
       <header className="App-header">
         <h2>{props.appName}</h2>
         <img src={memeUrl} />
-        <button onClick={fetchData}>Refresh</button>
+        <Button raised accent onClick={fetchData}>Refresh</Button>
       </header>
     </div>
   );
